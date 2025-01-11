6f018a65618f3d8082eca07adad0fa98e9bd7036
Saurabh Aggarwal <saurabha@saurabha-mlt.client.nvidia.com>
2025-01-10 17:17:08 -0800

    Fixing PR, to be in sync with upstream

--- cloud-service-providers/azure/azureml/cli/scripts/1_set_credentials.sh
+++ cloud-service-providers/azure/azureml/cli/scripts/1_set_credentials.sh
@@ -2,19 +2,19 @@
 set -x
 source config.sh
 
-# CREATE_WORKSPACE=false
+CREATE_WORKSPACE=false
 
-# for i in "$@"; do
-#   case $i in
-#     --create_new_workspace) CREATE_WORKSPACE=true ;;
-#     -*|--*) echo "Unknown option $i"; exit 1 ;;
-#   esac
-# done
+for i in "$@"; do
+  case $i in
+    --create_new_workspace) CREATE_WORKSPACE=true ;;
+    -*|--*) echo "Unknown option $i"; exit 1 ;;
+  esac
+done
 
-# # Create new workspace
-# if $CREATE_WORKSPACE; then
-#     az ml workspace create --name $workspace --resource-group $resource_group --location $location
-# fi
+# Create new workspace
+if $CREATE_WORKSPACE; then
+    az ml workspace create --name $workspace --resource-group $resource_group --location $location
+fi
 
 # Assign role permission to read secrets from workspace connections
 az role assignment create \
